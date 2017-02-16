89fc51d0037fc7eda04f7f24d1ccf6d1ee23b587
ccx19322 <chenchaoxu19322@163.com>
2017-02-16 10:57:52 +0800

    Initial Commit

---
+++ NavigationCover/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NavigationCover
+//
+//  Created by chen cx on 2017/2/16.
+//  Copyright © 2017年 chen cx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NavigationCover/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NavigationCover
+//
+//  Created by chen cx on 2017/2/16.
+//  Copyright © 2017年 chen cx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
